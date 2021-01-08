694512321726261d9b673ffde7c5adcb7eaec6c2
cynine <leizhangjie@gmail.com>
2021-01-08 11:09:32 +0800

    "clean"

--- android_shell/app/src/main/java/com/example/flutter_demo_module_ios/host/MainActivity.java
+++
@@ -1,7 +0,0 @@
-package com.example.flutter_demo_module_ios.host;
-
-import android.os.Bundle;
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
