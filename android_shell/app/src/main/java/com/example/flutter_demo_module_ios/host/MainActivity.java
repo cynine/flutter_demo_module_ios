3f0506a3f4064f69d0059b552cab1c8c5b9083c7
cynine <leizhangjie@gmail.com>
2021-01-08 10:09:41 +0800

    "init flutter"

---
+++ android_shell/app/src/main/java/com/example/flutter_demo_module_ios/host/MainActivity.java
@@ -0,0 +1,7 @@
+package com.example.flutter_demo_module_ios.host;
+
+import android.os.Bundle;
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
